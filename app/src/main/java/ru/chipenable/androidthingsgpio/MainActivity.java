ba6a5b9271f3c2fb823ab4830073930b26b54951
Pavel Bobkov <microsmaster@rambler.ru>
2017-06-25 04:27:51 +0300

    add button

--- app/src/main/java/ru/chipenable/androidthingsgpio/MainActivity.java
+++ app/src/main/java/ru/chipenable/androidthingsgpio/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManagerService;
 
 import java.io.IOException;
@@ -12,11 +13,33 @@ import java.io.IOException;
 public class MainActivity extends Activity {
 
     private static final String LED_PIN_NAME = "BCM5";
-    private static final long LED_PERIOD = 500;
+    private static final String BUT_PIN_NAME = "BCM6";
+    private static final long LED_PERIOD = 200;
     private static final String TAG = MainActivity.class.getName();
 
     private Gpio ledGpio;
+    private Gpio butGpio;
     private LedTask ledTask;
+    private boolean enableLed;
+
+    private GpioCallback buttonCallback = new GpioCallback() {
+
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            try {
+                enableLed = gpio.getValue();
+                return true;
+            } catch (IOException e) {
+                Log.d(TAG, e.toString());
+                return false;
+            }
+        }
+
+        @Override
+        public void onGpioError(Gpio gpio, int error) {
+            Log.w(TAG, gpio + ": Error event " + error);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +52,12 @@ public class MainActivity extends Activity {
             ledGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
             ledTask = new LedTask(ledGpio, LED_PERIOD);
             new Thread(ledTask).start();
+
+            butGpio = service.openGpio(BUT_PIN_NAME);
+            butGpio.setDirection(Gpio.DIRECTION_IN);
+            butGpio.setActiveType(Gpio.ACTIVE_LOW);
+            butGpio.setEdgeTriggerType(Gpio.EDGE_BOTH);
+            butGpio.registerGpioCallback(buttonCallback);
         }
         catch(IOException e){
             Log.d(TAG, "Error on PeripheralIO API", e);
@@ -43,14 +72,8 @@ public class MainActivity extends Activity {
             ledTask.stop();
         }
 
-        if (ledGpio != null){
-            try {
-                ledGpio.close();
-            }
-            catch(IOException e){
-                Log.d(TAG, e.toString());
-            }
-        }
+        releaseGpio(ledGpio, null);
+        releaseGpio(butGpio, buttonCallback);
     }
 
     private class LedTask implements Runnable{
@@ -78,14 +101,34 @@ public class MainActivity extends Activity {
                         return;
                     }
 
-                    boolean value = ledGpio.getValue();
-                    ledGpio.setValue(!value);
-                    Log.d(TAG, "led " + (value ? "on" : "off"));
+                    if (enableLed) {
+                        boolean value = ledGpio.getValue();
+                        ledGpio.setValue(!value);
+                        Log.d(TAG, "led " + (value ? "on" : "off"));
+                    }
+                    else{
+                        ledGpio.setValue(false);
+                    }
+
                     Thread.sleep(delay);
 
                 } catch (IOException | InterruptedException e) {
                     Log.d(TAG, e.toString());
                 }
+            }
+        }
+    }
+
+    private void releaseGpio(Gpio gpio, GpioCallback callback) {
+        if (gpio != null) {
+            try {
+                if (callback != null){
+                    gpio.unregisterGpioCallback(callback);
+                }
+                gpio.close();
+                gpio = null;
+            } catch (IOException e) {
+                Log.d(TAG, e.toString());
             }
         }
     }
